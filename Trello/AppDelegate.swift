5856fedacb59957e265468d2fecb72a9a3e61107
Alexis León <alexisunam1@hotmail.com>
2016-06-26 19:12:16 -0500

    Initial Commit

---
+++ Trello/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Trello
+//
+//  Created by Alexis Leon on 6/26/16.
+//  Copyright © 2016 Alexis Leon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Trello/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trello
+//
+//  Created by Alexis Leon on 6/26/16.
+//  Copyright © 2016 Alexis Leon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
